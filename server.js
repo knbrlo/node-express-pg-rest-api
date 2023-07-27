e030b5bce79fb6aa01251415ba08eae3d93fc43a
Ken Barlow <passing-oxtail0a@icloud.com>
2023-07-26 18:51:23 -0600

    Able to get all books.

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const pgp = require('pg-promise')();
+const db = pgp({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});
+
+
+// GET all books
+app.get('/books', (req, res) => {
+    db.any('SELECT * FROM books')
+    .then(data => {
+        res.json({ data });
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
+
+// GET a single book by id
+
+// POST a new book
+
+// PUT update a book
+
+// DELETE a book
+
+// Start the server
+app.listen(port, () => console.log(`Server running on port ${port}`));
